2ea19d5589c7753423a4030bb0aca02e3e31526c
famouskaykay <86409796+famouskaykay@users.noreply.github.com>
2021-08-10 22:08:21 +0300

    Update repo.py

--- wbb/modules/repo.py
+++ wbb/modules/repo.py
@@ -1,26 +1,4 @@
-"""
-MIT License
 
-Copyright (c) 2021 TheHamkerCat
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
-"""
 from pyrogram import filters
 
 from wbb import app
@@ -29,8 +7,8 @@ from wbb.utils.http import get
 
 __MODULE__ = "Repo"
 __HELP__ = (
-    "/repo - To Get My Github Repository Link "
-    "And Support Group Link"
+    "/repo - kupata github repo yangu "
+    "na support group "
 )
 
 
@@ -38,7 +16,7 @@ __HELP__ = (
 @capture_err
 async def repo(_, message):
     users = await get(
-        "https://api.github.com/repos/thehamkercat/williambutcherbot/contributors"
+        "https://api.github.com/repos/famouskaykay/raiya"
     )
     list_of_users = ""
     count = 1
@@ -48,7 +26,7 @@ async def repo(_, message):
         )
         count += 1
 
-    text = f"""[Github](https://github.com/thehamkercat/WilliamButcherBot) | [Group](t.me/PatheticProgrammers)
+    text = f"""[Github](https://github.com/famouskaykay/raiya) | [Group](t.me/KayAspirerProject)
 ```----------------
 | Contributors |
 ----------------```
