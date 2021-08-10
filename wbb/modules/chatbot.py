69b7dedfea5c14a99dd142147d6a576e47289835
famouskaykay <86409796+famouskaykay@users.noreply.github.com>
2021-08-10 23:00:38 +0300

    Update chatbot.py

--- wbb/modules/chatbot.py
+++ wbb/modules/chatbot.py
@@ -1,26 +1,3 @@
-"""
-MIT License
-
-Copyright (c) 2021 TheHamkerCat
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
-"""
 from asyncio import gather, sleep
 
 from pyrogram import filters
@@ -50,7 +27,7 @@ async def chat_bot_toggle(db, message: Message):
             text = "Chatbot Enabled!"
             return await edit_or_reply(message, text=text)
         await edit_or_reply(
-            message, text="ChatBot Is Already Enabled."
+            message, text="ChatBot iko Enabled."
         )
     elif status == "disable":
         if chat_id in db:
