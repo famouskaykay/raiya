9237311a5f05abed547ab4643c6aa1abd014f051
famouskaykay <86409796+famouskaykay@users.noreply.github.com>
2021-08-10 22:48:57 +0300

    Update misc.py

--- wbb/modules/misc.py
+++ wbb/modules/misc.py
@@ -1,26 +1,3 @@
-"""
-MIT License
-
-Copyright (c) 2021 TheHamkerCat
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
-"""
 import secrets
 import string
 from asyncio import Lock
@@ -40,7 +17,7 @@ __MODULE__ = "Misc"
 __HELP__ = """
 /asq - Ask a question
 /commit - Generate Funny Commit Messages
-/runs  - Idk Test Yourself
+/runs  - sijui ebu jaribu
 /id - Get Chat_ID or User_ID
 /random [Length] - Generate Random Complex Passwords
 /encrypt - Encrypt Text [Can Only Be Decrypted By This Bot]
