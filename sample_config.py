4b0a499646f174c525ef45f4d9ca2accd4cc697b
daveh566 <69384676+daveh566@users.noreply.github.com>
2021-08-14 08:01:10 +0300

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -51,3 +51,32 @@ else:
     LOG_MENTIONS = True
     RSS_DELAY = 300  # In seconds
     PM_PERMIT = True
+
+def get_str_key(name, required=False):
+    if name in DEFAULTS:
+        default = DEFAULTS[name]
+    else:
+        default = None
+    if not (data := env.str(name, default=default)) and not required:
+        log.warn("No str key: " + name)
+        return None
+    elif not data:
+        log.critical("No str key: " + name)
+        sys.exit(2)
+    else:
+        return data
+
+
+def get_int_key(name, required=False):
+    if name in DEFAULTS:
+        default = DEFAULTS[name]
+    else:
+        default = None
+    if not (data := env.int(name, default=default)) and not required:
+        log.warn("No int key: " + name)
+        return None
+    elif not data:
+        log.critical("No int key: " + name)
+        sys.exit(2)
+    else:
+        return data
