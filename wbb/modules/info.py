065e97cc0d5636885eef9108a621e4e83b3264ba
famouskaykay <86409796+famouskaykay@users.noreply.github.com>
2021-08-15 05:22:39 +0300

    Update info.py

--- wbb/modules/info.py
+++ wbb/modules/info.py
@@ -1,26 +1,3 @@
-"""
-MIT License
-
-Copyright (c) 2021 TheHamkerCat
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
-"""
 import os
 
 from pyrogram import filters
@@ -38,8 +15,9 @@ __HELP__ = """
 """
 
 
-async def get_user_info(user):
-    user = await app.get_users(user)
+async def get_user_info(user, already=False):
+    if not already:
+        user = await app.get_users(user)
     if not user.first_name:
         return ["Deleted account", None]
     user_id = user.id
@@ -65,8 +43,9 @@ async def get_user_info(user):
     return [caption, photo_id]
 
 
-async def get_chat_info(chat):
-    chat = await app.get_chat(chat)
+async def get_chat_info(chat, already=False):
+    if not already:
+        chat = await app.get_chat(chat)
     chat_id = chat.id
     username = chat.username
     title = chat.title
