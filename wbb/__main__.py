bafa68a295f51a2ec1c3ec6b1ed5725e4150ea99
famouskaykay <86409796+famouskaykay@users.noreply.github.com>
2021-08-22 23:03:20 +0300

    Update __main__.py

--- wbb/__main__.py
+++ wbb/__main__.py
@@ -114,58 +114,6 @@ home_text_pm = (
     + "add me to your group."
 )
 
-@Client.on_message(filters.private & filters.incoming & filters.command(['start']))
-def _start(client, message):
-    client.send_message(message.chat.id,
-        text=tr.START_MSG.format(message.from_user.first_name, message.from_user.id),
-        parse_mode="markdown",
-        reply_to_message_id=message.message_id
-        )
-
-
-@Client.on_message(filters.private & filters.incoming & filters.command(['help']))
-def _help(client, message):
-    client.send_message(chat_id = message.chat.id,
-        text = tr.HELP_MSG[1],
-        parse_mode="markdown",
-        disable_notification = True,
-        reply_markup = InlineKeyboardMarkup(map(1)),
-        reply_to_message_id = message.message_id
-    )
-
-help_callback_filter = filters.create(lambda _, __, query: query.data.startswith('help+'))
-
-@Client.on_callback_query(help_callback_filter)
-def help_answer(client, callback_query):
-    chat_id = callback_query.from_user.id
-    message_id = callback_query.message.message_id
-    msg = int(callback_query.data.split('+')[1])
-    client.edit_message_text(chat_id=chat_id,    message_id=message_id,
-        text=tr.HELP_MSG[msg],    reply_markup=InlineKeyboardMarkup(map(msg))
-    )
-
-
-def map(pos):
-    if(pos==1):
-        button = [
-            [InlineKeyboardButton(text = 'Next', callback_data = "help+2")]
-        ]
-    elif(pos==len(tr.HELP_MSG)-1):
-        url = "https://github.com/famouskaykay/raiya"
-        button = [
-            [InlineKeyboardButton(text = 'Support Us', url="https://t.me/KayAspirerProject")],
-            [InlineKeyboardButton(text = 'Feature Request & Issues', url=url)],
-            [InlineKeyboardButton(text = 'Back', callback_data = f"help+{pos-1}")]
-        ]
-    else:
-        button = [
-            [
-                InlineKeyboardButton(text = 'Back', callback_data = f"help+{pos-1}"),
-                InlineKeyboardButton(text = 'Next', callback_data = f"help+{pos+1}")
-            ],
-        ]
-    return button
-
     
 @app.on_message(filters.command(["help", "start"]))
 async def help_command(_, message):
