3100c5db6a14c261a7c6538dc6c421854e57bad8
famouskaykay <86409796+famouskaykay@users.noreply.github.com>
2021-08-22 13:41:48 +0300

    Update forcesubscribe.py

--- wbb/modules/forcesubscribe.py
+++ wbb/modules/forcesubscribe.py
@@ -185,9 +185,8 @@ def config(client, message):
                 except (UsernameNotOccupied, PeerIdInvalid):
                     message.reply_text(f"**Invalid Channel Username.**")
                 except Exception as err:
-                    message.reply_text(f"**ERROR:**
-                                       {err}
-                                       ")
+                    message.reply_text(f"ERROR:
+                                       {err}")
         else:
             if sql.fs_settings(chat_id):
                 message.reply_text(
