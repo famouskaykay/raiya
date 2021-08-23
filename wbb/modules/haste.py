21d64c0ce5e6a7746930713c6cfa0628a7097730
famouskaykay <86409796+famouskaykay@users.noreply.github.com>
2021-08-23 23:22:07 +0300

    Create haste.py

---
+++ wbb/modules/haste.py
@@ -0,0 +1,23 @@
+import requests
+
+from pyrogram import Client, Filters
+
+BASE = "https://hastebin.com"
+
+
+@Client.on_message(Filters.command("haste", prefix="!") & Filters.reply & ~Filters.edited & Filters.group)
+def haste(client, message):
+    reply = message.reply_to_message
+
+    if reply.text is None:
+        return
+
+    result = requests.post(
+        "{}/documents".format(BASE),
+        data=reply.text.encode("UTF-8")
+    ).json()
+
+    message.reply(
+        "{}/{}.py".format(BASE, result["key"]),
+        reply_to_message_id=reply.message_id
+    )
