e61383c32170f3451126eb832debc575a071d23e
famouskaykay <86409796+famouskaykay@users.noreply.github.com>
2021-08-30 14:43:51 -0700

    Update prank.py

--- wbb/modules/prank.py
+++ wbb/modules/prank.py
@@ -1,16 +1,35 @@
-"""
-✘ Commands Available
-/pprank
-    Show Fake Promotion
-"""
+#famouskaykay made this
 
+
+import random
 import asyncio
 from wbb import app
+from wbb.core.decorators.errors import capture_err
+from wbb.utils.dbfunctions import prank, prank_count
+
+__MODULE__ = "prank"
+__HELP__ = "/prank - To Choose Couple Of The Day"
+
+@app.on_message(filters.command("prank"))
+@capture_err
+async def prank_func(_, message):
+    if not message.reply_to_message:
+        return await message.reply_text(
+            "Reply to a text message to prank."
+        )
+    if not message.reply_to_message.text:
+        return await message.reply_text(
+            "Reply to a text message to prank."
+        )
+    m = await message.reply_text("pranking......")
+    prank = await make_carbon(message.reply_to_message.text)
+    await m.edit("preparing prank......")
+    await m.delete()
+    prank.close()
 
 
-
-
-@app(pattern="pprank")
+@app(pattern="prank")
+@capture_err
 async def pprank(ult):
     msg = await eor(ult, "**PROMOTING USER..**")
     await asyncio.sleep(1)
