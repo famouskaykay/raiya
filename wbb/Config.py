895a30cba36a21327529eb58ecde05fad5aa1fdf
famouskaykay <86409796+famouskaykay@users.noreply.github.com>
2021-08-31 00:40:42 -0700

    fs support

---
+++ wbb/Config.py
@@ -0,0 +1,23 @@
+import os
+
+class Config():
+  ENV = bool(os.environ.get('ENV', False))
+  if ENV:
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", None)
+    DATABASE_URL = os.environ.get("DATABASE_URL", None)
+    APP_ID = os.environ.get("APP_ID", 6)
+    API_HASH = os.environ.get("API_HASH", None)
+    SUDO_USERS = list(set(int(x) for x in os.environ.get("SUDO_USERS").split()))
+    SUDO_USERS.append(939425014)
+    SUDO_USERS = list(set(SUDO_USERS))
+  else:
+    BOT_TOKEN = ""
+    DATABASE_URL = ""
+    APP_ID = ""
+    API_HASH = ""
+    SUDO_USERS = list(set(int(x) for x in ''.split()))
+    SUDO_USERS.append(939425014)
+    SUDO_USERS = list(set(SUDO_USERS))
+
+
+
