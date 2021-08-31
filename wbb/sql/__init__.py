62b966da7cf7b7f895bb5b47c2c72385b59913cb
famouskaykay <86409796+famouskaykay@users.noreply.github.com>
2021-08-31 00:34:27 -0700

    Create __init__.py

---
+++ wbb/sql/__init__.py
@@ -0,0 +1,20 @@
+import os
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker, scoped_session
+from wbb.Config import Config
+
+def start() -> scoped_session:
+    engine = create_engine(Config.DATABASE_URL)
+    BASE.metadata.bind = engine
+    BASE.metadata.create_all(engine)
+    return scoped_session(sessionmaker(bind=engine, autoflush=False))
+
+
+try:
+    BASE = declarative_base()
+    SESSION = start()
+except AttributeError as e:
+    # yeah
+    print("DATABASE_URL is not configured. Features depending on the database might have issues.")
+    print(str(e))
