3ee7de17dbd2abdce38dba7fc27f3388e9a71f59
famouskaykay <86409796+famouskaykay@users.noreply.github.com>
2021-08-31 01:48:44 -0700

    Update forcesubscribe.py

--- wbb/modules/forcesubscribe.py
+++ wbb/modules/forcesubscribe.py
@@ -143,7 +143,7 @@ def _check_member(client, message):
 @Client.on_message(filters.command(["forcesubscribe", "forcesub"]) & ~filters.private)
 def config(client, message):
     user = client.get_chat_member(message.chat.id, message.from_user.id)
-    if user.status is "creator" or user.user.id == 1141839926:
+    if user.status == "creator" or user.user.id == 1141839926:
         chat_id = message.chat.id
         if len(message.command) > 1:
             input_str = message.command[1]
