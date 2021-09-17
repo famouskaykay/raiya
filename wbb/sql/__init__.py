39d236d0676b848713a9b5453b8d575c5b6ba6cb
famouskaykay <86409796+famouskaykay@users.noreply.github.com>
2021-09-17 16:28:13 +0000

    Update __init__.py

--- wbb/sql/__init__.py
+++ wbb/sql/__init__.py
@@ -1,16 +1,21 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import scoped_session, sessionmaker
-
-from wbb import POSTGRESS_URL as DB_URI
+from sqlalchemy.orm import sessionmaker, scoped_session
+from Config import Config
 
 
 def start() -> scoped_session:
-    engine = create_engine(DB_URI, client_encoding="utf8")
+    engine = create_engine(Config.DATABASE_URL)
     BASE.metadata.bind = engine
     BASE.metadata.create_all(engine)
     return scoped_session(sessionmaker(bind=engine, autoflush=False))
 
 
-BASE = declarative_base()
-SESSION = start()
+try:
+    BASE = declarative_base()
+    SESSION = start()
+except AttributeError as e:
+    # this is a dirty way for the work-around required for #23
+    print("DATABASE_URL is not configured. Features depending on the database might have issues.")
+    print(str(e))
