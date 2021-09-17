2e3e34eeb4dff3ae22ea7388a3ab03805d2103d5
famouskaykay <86409796+famouskaykay@users.noreply.github.com>
2021-09-17 19:02:28 +0000

    Update forcesubscribe.py

--- wbb/modules/forcesubscribe.py
+++ wbb/modules/forcesubscribe.py
@@ -2,7 +2,6 @@ import logging
 import time
 
 from pyrogram import Client
-from wbb.Config import Config
 from wbb.core.decorators.errors import capture_err
 from pyrogram.errors import RPCError
 from pyrogram.errors.exceptions.bad_request_400 import UserNotParticipant, UsernameNotOccupied, ChatAdminRequired, PeerIdInvalid
@@ -14,70 +13,10 @@ from pyrogram.types import (Chat, ChatPermissions,
                             InlineKeyboardMarkup, Message, User)
 from wbb import SUDOERS
 
-from wbb.sql import forceSubscribe_sql as sql
 from wbb import app
 from wbb import BOT_ID
 from wbb import db
 import time
-
-
-@app.on_callback_query(filters.regex("^onButtonPress$"))
-def onButtonPress(client, cb):
-  user_id = member.id
-  chat_id = message.chat.id
-  cws = sql.fs_settings(chat_id)
-  if cws:
-    channel = cws.channel
-    if client.get_chat_member(chat_id, user_id).restricted_by.id == (client.get_me()).id:
-      try:
-        client.get_chat_member(channel, user_id)
-        client.unban_chat_member(chat_id, user_id)
-      except UserNotParticipant:
-        client.answer_callback_query(cb.id, text="ℹ Join The Channel And Press The Button Again.")
-    else:
-      client.answer_callback_query(cb.id, text="ℹ You Are Muted By Admins For Other Reasons.", show_alert=True)
-
-    
-
-    
-@app.on_message(filters.command(["forcesubscribe", "fsub"]) & ~Filters.private)
-def kay(client, message):
-  if User.status is "creator" or User.user.id in SUDOERS:
-    chat_id = message.chat.id
-    if len(message.command) > 1:
-      input_str = message.command[1]
-      input_str = input_str.replace("@", "")
-      if input_str.lower() in ("off", "no", "disable"):
-        sql.disapprove(chat_id)
-        message.reply_text("❌ **Force Subscribe is Disabled Successfully.**")
-      elif input_str.lower() in ('clear'):
-        sent_message = message.reply_text('**🔔 Unmuting All Members Who is Muted By Me...**')
-        try:
-          for chat_member in client.get_chat_members(message.chat.id, filter="restricted"):
-            if chat_member.restricted_by.id == (client.get_me()).id:
-                client.unban_chat_member(chat_id, chat_member.user.id)
-                time.sleep(1)
-          sent_message.edit('✅ **UnMuted All Members Which Are Muted By Me.**')
-        except ChatAdminRequired:
-          sent_message.edit('❗ **I Am Not An Admin In This Chat.**\nI Can not Unmute Members Because I Am Not An Admin in This Chat, Make Me.')
-      else:
-        try:
-          client.get_chat_member(input_str, "me")
-          sql.add_channel(chat_id, input_str)
-          message.reply_text(f"✅ **Force Subscribe is Enabled**\nForce Subscribe is Enabled, All The Group Members Have To Subscribe This [channel](https://t.me/{input_str}) in Order To Send Messages In This Group.", disable_web_page_preview=True)
-        except UserNotParticipant:
-          message.reply_text(f"❗ **Not An Admin in The Channel**\nI'm Not An Admin in The [channel](https://t.me/{input_str}). Add Me As A Admin In Order To Enable ForceSubscribe.", disable_web_page_preview=True)
-        except (UsernameNotOccupied, PeerIdInvalid):
-          message.reply_text(f"❗ **Invalid Channel Username.**")
-        except Exception as err:
-          message.reply_text(f"❗ **ERROR:** ```{err}```")
-    else:
-      if sql.fs_settings(chat_id):
-        message.reply_text(f"✅ **Force Subscribe is Enabled in This Chat.**\nFor This [Channel](https://t.me/{sql.fs_settings(chat_id).channel})", disable_web_page_preview=True)
-      else:
-        message.reply_text("❌ **Force Subscribe is Disabled in This Chat.**")
-  else:
-      message.reply_text("❗ **Group Creator Required**\nYou Have To Be The Group Creator To Do That.")
 
 
 
