64bd5958e3de23798913d20baca574798877c6b0
famouskaykay <86409796+famouskaykay@users.noreply.github.com>
2021-09-19 17:37:17 +0000

    Update __init__.py

--- wbb/sql/__init__.py
+++ wbb/sql/__init__.py
@@ -1,21 +1,15 @@
-import os
+from wbb import DB_URI
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker, scoped_session
-from wbb.Config import Config
+from sqlalchemy.orm import scoped_session, sessionmaker
 
 
 def start() -> scoped_session:
-    engine = create_engine(Config.DATABASE_URL)
+    engine = create_engine(DB_URI, client_encoding="utf8")
     BASE.metadata.bind = engine
     BASE.metadata.create_all(engine)
     return scoped_session(sessionmaker(bind=engine, autoflush=False))
 
 
-try:
-    BASE = declarative_base()
-    SESSION = start()
-except AttributeError as e:
-    # this is a dirty way for the work-around required for #23
-    print("DATABASE_URL is not configured. Features depending on the database might have issues.")
-    print(str(e))
+BASE = declarative_base()
+SESSION = start()
