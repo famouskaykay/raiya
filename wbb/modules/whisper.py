0066c45384816531d003f3c5c7c2f187f37a0134
famouskaykay <86409796+famouskaykay@users.noreply.github.com>
2021-09-29 18:15:11 +0000

    Update whisper.py

--- wbb/modules/whisper.py
+++ wbb/modules/whisper.py
@@ -1,87 +1,148 @@
-#copy right @aspirer2 2021
-from telethon import events, TelegramClient, Button
-import logging
-from telethon.tl.functions.users import GetFullUserRequest as us
-import os
-from wbb import app
+from typing import Optional
 
-db = {}
+from pyrogram import Client, filters, emoji
+from pyrogram.errors.exceptions.bad_request_400 import (
+    MessageIdInvalid, MessageNotModified
+)
+from pyrogram.types import (
+    User,
+    InlineQuery,
+    InlineQueryResultArticle,
+    InputTextMessageContent, InlineKeyboardMarkup, InlineKeyboardButton,
+    CallbackQuery,
+    ChosenInlineResult
+)
 
-@app.on(events.InlineQuery())
-async def die(event):
-    if len(event.text) != 0:
-        return
-    me = (await app.get_me()).username
-    dn = event.builder.article(
-            title="It's a whisper!",
-            description="It's a whisper!\n(c) @KayAspirerProject",
-            text=f"**I can a whisper messages**\n`@{me} wspr UserID|Message`\n**(c) @KayAspirerProject**",
-            buttons=[
-                [Button.switch_inline(" Go Inline ", query="wspr ")]
-                ]
+from wbb.data import whispers
+
+# https://core.telegram.org/bots/api#answercallbackquery
+# https://core.telegram.org/bots/api#callbackquery
+ANSWER_CALLBACK_QUERY_MAX_LENGTH = 200
+
+# https://www.freeiconspng.com/downloadimg/37535
+WHISPER_ICON_URL = "https://www.freeiconspng.com/uploads/whisper-icon-0.png"
+
+
+@Client.on_inline_query()
+async def answer_iq(_, iq: InlineQuery):
+    query = iq.query
+    split = query.split(' ', 1)
+    if query == '' or len(query) > ANSWER_CALLBACK_QUERY_MAX_LENGTH \
+            or (query.startswith('@') and len(split) == 1):
+        title = f"{emoji.FIRE} Write a whisper message"
+        content = ("**Send whisper messages through inline mode**\n\n"
+                   "Usage: `@ezWhisperBot [@username|@] text`")
+        description = "Usage: @ezWhisperBot [@username|@] text"
+        button = InlineKeyboardButton(
+            "Learn more...",
+            url="https://t.me/ezWhisperBot?start=learn"
+        )
+    elif not query.startswith('@'):
+        title = f"{emoji.EYE} Whisper once to the first one who open it"
+        content = (
+            f"{emoji.EYE} The first one who open the whisper can read it"
+        )
+        description = f"{emoji.SHUSHING_FACE} {query}"
+        button = InlineKeyboardButton(
+            f"{emoji.EYE} show message",
+            callback_data="show_whisper"
+        )
+    else:
+        # Python 3.8+
+        u_target = 'anyone' if (x := split[0]) == '@' else x
+        title = f"{emoji.LOCKED} A whisper message to {u_target}"
+        content = f"{emoji.LOCKED} A whisper message to {u_target}"
+        description = f"{emoji.SHUSHING_FACE} {split[1]}"
+        button = InlineKeyboardButton(
+            f"{emoji.LOCKED_WITH_KEY} show message",
+            callback_data="show_whisper"
+        )
+    switch_pm_text = f"{emoji.INFORMATION} Learn how to send whispers"
+    switch_pm_parameter = "learn"
+    await iq.answer(
+        results=[
+            InlineQueryResultArticle(
+                title=title,
+                input_message_content=InputTextMessageContent(content),
+                description=description,
+                thumb_url=WHISPER_ICON_URL,
+                reply_markup=InlineKeyboardMarkup([[button]])
             )
-    await event.answer([dn])
-    
-@app.on(events.InlineQuery(pattern="wspr"))
-async def inline(event):
-    me = (await app.get_me()).username
+        ],
+        switch_pm_text=switch_pm_text,
+        switch_pm_parameter=switch_pm_parameter
+    )
+
+
+@Client.on_chosen_inline_result()
+async def chosen_inline_result(_, cir: ChosenInlineResult):
+    query = cir.query
+    split = query.split(' ', 1)
+    len_split = len(split)
+    if len_split == 0 or len(query) > ANSWER_CALLBACK_QUERY_MAX_LENGTH \
+            or (query.startswith('@') and len(split) == 1):
+        return
+    if len_split == 2 and query.startswith('@'):
+        # Python 3.9+
+        # receiver_uname, text = split[0].removeprefix('@'), split[1]
+        receiver_uname, text = split[0][1:] or '@', split[1]
+    else:
+        receiver_uname, text = None, query
+    sender_uid = cir.from_user.id
+    inline_message_id = cir.inline_message_id
+    whispers[inline_message_id] = {
+        'sender_uid': sender_uid,
+        'receiver_uname': receiver_uname,
+        'text': text
+    }
+
+
+@Client.on_callback_query(filters.regex("^show_whisper$"))
+async def answer_cq(_, cq: CallbackQuery):
+    inline_message_id = cq.inline_message_id
+    if not inline_message_id or inline_message_id not in whispers:
+        try:
+            await cq.answer("Can't find the whisper text", show_alert=True)
+            await cq.edit_message_text(f"{emoji.NO_ENTRY} invalid whisper")
+        except (MessageIdInvalid, MessageNotModified):
+            pass
+        return
+    else:
+        whisper = whispers[inline_message_id]
+        sender_uid = whisper['sender_uid']
+        receiver_uname: Optional[str] = whisper['receiver_uname']
+        whisper_text = whisper['text']
+        from_user: User = cq.from_user
+        if receiver_uname and from_user.username \
+                and from_user.username.lower() == receiver_uname.lower():
+            await read_the_whisper(cq)
+            return
+        if from_user.id == sender_uid or receiver_uname == '@':
+            await cq.answer(whisper_text, show_alert=True)
+            return
+        if not receiver_uname:
+            await read_the_whisper(cq)
+            return
+        await cq.answer("This is not for you", show_alert=True)
+
+
+async def read_the_whisper(cq: CallbackQuery):
+    inline_message_id = cq.inline_message_id
+    whisper = whispers[inline_message_id]
+    whispers.pop(inline_message_id, None)
+    whisper_text = whisper['text']
+    await cq.answer(whisper_text, show_alert=True)
+    receiver_uname: Optional[str] = whisper['receiver_uname']
+    from_user: User = cq.from_user
+    user_mention = (
+        f"{from_user.first_name} (@{from_user.username})"
+        if from_user.username
+        else from_user.mention
+    )
     try:
-        inp = event.text.split(None, 1)[1]
-        user, msg = inp.split("|")
-    except IndexError:
-        await event.answer(
-                [], 
-                switch_pm=f"@{me} [UserID]|[Message]",
-                switch_pm_param="start"
-                )
-    except ValueError:
-        await event.answer(
-                [],
-                switch_pm=f"Give a message too!",
-                switch_pm_param="start"
-                )
-    try:
-        ui = await app(us(user))
-    except BaseException:
-        await event.answer(
-                [],
-                switch_pm="Invalid User ID/Username",
-                switch_pm_param="start"
-                )
-        return
-    db.update({"user_id": ui.user.id, "msg": msg, "self": event.sender.id})
-    text = f"""
-A Whisper Has Been Sent
-To [{ui.user.first_name}](tg://user?id={ui.user.id})!
-Click The Below Button To See The Message!
-**Note:** __Only {ui.user.first_name} can open this!__
-    """
-    dn = event.builder.article(
-            title="Its a secret message! Sssh",
-            description="It's a secret message! Sssh!",
-            text=text,
-            buttons=[
-                [Button.inline(" Show Message! ", data="wspr")]
-                ]
-            )
-    await event.answer(
-            [dn],
-            switch_pm="It's a secret message! Sssh",
-            switch_pm_param="start"
-            )
-
-
-@app.on(events.CallbackQuery(data="wspr"))
-async def ws(event):
-    user = int(db["user_id"])
-    lol = [int(db["self"])]
-    lol.append(user)
-    if event.sender.id not in lol:
-        await event.answer("🔐 This message is not for you!", alert=True)
-        return
-    msg = db["msg"]
-    if msg == []:
-        await event.anwswer(
-                "Oops!\nIt's looks like message got deleted from my server!", alert=True)
-        return
-    await event.answer(msg, alert=True)
+        t_emoji = emoji.UNLOCKED if receiver_uname else emoji.EYES
+        await cq.edit_message_text(
+            f"{t_emoji} {user_mention} read the message"
+        )
+    except (MessageIdInvalid, MessageNotModified):
+        pass
