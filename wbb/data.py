e4fc1505ee4867ee4c7ecffbcc4d6a28f3d2fe85
famouskaykay <86409796+famouskaykay@users.noreply.github.com>
2021-09-30 21:13:12 +0000

    Delete data.py

--- wbb/data.py
+++
@@ -1,9 +0,0 @@
-#whisper support
-
-import json
-try:
-    with open('data.json') as f:
-        whispers = json.load(f)
-except (FileNotFoundError, json.JSONDecodeError):
-    whispers = {}
-open('data.json', 'w').close()
